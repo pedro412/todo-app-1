a4e735b07296fb44a929451c2a3bfb1c872eb9f1
pedro412 <pedro.alvarez412@outlook.com>
2021-01-29 23:07:57 -0600

    kbeza

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-console.log('Holaperros');
+const todoList = ['Learn web', 'Pay attention'];
+const htmlList = document.getElementById('list-html');
+const submitButton = document.getElementById('submit');
 
-let list = ['pachi', 'chona', 'chuky'];
+const addItemsToList = (todoList) => {
+  htmlList.innerHTML = '';
+  todoList.forEach((todo) => {
+    htmlList.innerHTML += `<li>${todo} <span>X</span></li>`;
+  });
+};
+
+addItemsToList(todoList);
+
+submitButton.addEventListener('click', () => {
+  const valor = 'adfs';
+  todoList.push(valor);
+  addItemsToList(todoList);
+});
